e1d788139ffe45266af921b14e6b271479e323ff
Matthias Luger <malua007@live.de>
2022-12-06 14:24:36 +0100

    day 6

---
+++ 2022/day6/p1.js
@@ -0,0 +1,26 @@
+const fs = require('fs')
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+
+    for (let i = 3; i < data.length - 1; i++) {
+        if (!checkForDuplicates(data.substring(i - 3, i + 1))) {
+            console.log(i + 1)
+            return
+        }
+    }
+})
+
+function checkForDuplicates(string) {
+    let duplicate = false
+    for (let i = 0; i < string.length; i++) {
+        const element = string[i]
+        if (string.lastIndexOf(element) !== i || string.indexOf(element) !== i) {
+            duplicate = true
+        }
+    }
+    return duplicate
+}
