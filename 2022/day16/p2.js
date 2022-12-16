e013838ccad333c7a9f0739c96f9b9462ad97071
Matthias Luger <malua007@live.de>
2022-12-16 20:30:16 +0100

    day 16

---
+++ 2022/day16/p2.js
@@ -0,0 +1,107 @@
+const fs = require('fs')
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    let lines = data.split('\r\n')
+    let nodesMap = {}
+    lines.forEach(line => {
+        let name = line.split('Valve ')[1].split(' has ')[0]
+        let rate = +line.split('flow rate=')[1].split(';')[0]
+        let leadsTo = line.indexOf('to valves ') !== -1 ? line.split('to valves ')[1].split(', ') : [line.split('to valve ')[1]]
+
+        nodesMap[name] = { leadsTo, rate, name }
+    })
+
+    let paths = findPossiblePaths(nodesMap, 26)
+    let max = 0
+
+    for (let i = 0; i < paths.length; i++) {
+        for (let j = i + 1; j < paths.length; j++) {
+            if (!intersectPaths(paths[i], paths[j])) {
+                if (paths[i].pressure + paths[j].pressure > max) {
+                    max = paths[i].pressure + paths[j].pressure
+                }
+            }
+        }
+    }
+    console.log(max)
+})
+
+function intersectPaths(pathA, pathB) {
+    let intersect = false
+
+    pathA.path.forEach(p => {
+        if (pathB.path.includes(p)) {
+            intersect = true
+        }
+    })
+
+    return intersect
+}
+
+function getActiveNodes(nodesMap) {
+    return Object.keys(nodesMap)
+        .map(key => nodesMap[key])
+        .filter(node => node.rate > 0)
+}
+
+function getDistances(nodes, startName) {
+    let distances = {}
+    if (nodes[startName].distanceMap) {
+        return nodes[startName].distanceMap
+    }
+    function calcDistances(name, steps) {
+        if (distances[name] != undefined && distances[name] <= steps) {
+            return
+        }
+        distances[name] = steps
+        nodes[name].leadsTo.forEach(n => calcDistances(n, steps + 1))
+    }
+    calcDistances(startName, 0)
+    nodes[startName].distanceMap = distances
+    return distances
+}
+
+function findPossiblePaths(nodesMap, time) {
+    let paths = [{ curr: 'AA', active: getActiveNodes(nodesMap).map(n => n.name), timeLeft: time, finished: false, path: [], pressure: 0 }]
+    let max = 0
+
+    for (let n = 0; n < paths.length; n++) {
+        let path = paths[n]
+        if (path.timeLeft <= 0) {
+            path.finished = true
+        }
+        if (path.finished) {
+            continue
+        }
+
+        let distances = getDistances(nodesMap, path.curr)
+        let moved = false
+
+        path.active.forEach(act => {
+            if (act == path.curr || path.timeLeft - distances[act] <= 1) {
+                return true
+            }
+            moved = true
+            paths.push({
+                curr: act,
+                active: path.active.filter(v => v != act),
+                timeLeft: path.timeLeft - distances[act] - 1,
+                finished: false,
+                path: [...path.path, act],
+                pressure: path.pressure + (path.timeLeft - distances[act] - 1) * nodesMap[act].rate
+            })
+        })
+        if (!moved) {
+            path.finished = true
+        }
+        if (path.finished && path.pressure > max) {
+            max = path.pressure
+        }
+    }
+
+    return paths.filter(p => p.finished).sort((a, b) => b.pressure - a.pressure)
+}
