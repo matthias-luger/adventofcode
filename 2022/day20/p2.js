e6928560927bed6b360558b17216f877c26c1ab0
Matthias Luger <malua007@live.de>
2022-12-26 07:36:41 +0100

    day 20

--- 2022/day20/p2.js
+++ 2022/day20/p2.js
@@ -1,23 +1,25 @@
 const fs = require('fs')
 
+const key = 811589153
+
 fs.readFile('./input.txt', 'utf8', (err, data) => {
     if (err) {
         console.error(err)
         return
     }
     let split = data.split('\r\n')
-    let original = []
     let list = []
     split.forEach((value, i) => {
-        original.push(parseInt(value))
-        list.push({ value: parseInt(value), originalIndex: i })
+        list.push({ value: parseInt(value) * 811589153, originalIndex: i })
     })
 
-    for (let i = 0; i < list.length; i++) {
-        let listIndex = list.findIndex(entry => entry.originalIndex === i)
-        let value = list[listIndex].value
-        list.splice(listIndex, 1)
-        list.splice((listIndex + value) % list.length, 0, { value, originalIndex: i })
+    for (let mix = 0; mix < 10; mix++) {
+        for (let i = 0; i < list.length; i++) {
+            let listIndex = list.findIndex(entry => entry.originalIndex === i)
+            let value = list[listIndex].value
+            list.splice(listIndex, 1)
+            list.splice((listIndex + value) % list.length, 0, { value, originalIndex: i })
+        }
     }
 
     let indexOfZero = list.findIndex(e => e.value === 0)
