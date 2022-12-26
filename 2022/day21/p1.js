7999ab2016d779bb1bb546964d17d2155814b922
Matthias Luger <malua007@live.de>
2022-12-26 08:54:18 +0100

    day 21

--- 2022/day21/p1.js
+++ 2022/day21/p1.js
@@ -1,10 +1,28 @@
 const fs = require('fs')
 
-fs.readFile('./sample.txt', 'utf8', (err, data) => {
+fs.readFile('./input.txt', 'utf8', (err, data) => {
     if (err) {
         console.error(err)
         return
     }
-    let split = data.split('\r\n')
-    split.forEach(value => {})
+    let lines = data.split('\r\n')
+    let monkeyMap = new Map()
+
+    lines.forEach(line => {
+        let name = line.split(':')[0]
+        let split = line.split(' ')
+        let getYell = function () {
+            if (!isNaN(+split[1])) {
+                return +split[1]
+            }
+            let name1 = split[1]
+            let name2 = split[3]
+            let operator = split[2]
+
+            return eval(`${monkeyMap.get(name1)()} ${operator} ${monkeyMap.get(name2)()}`)
+        }
+        monkeyMap.set(name, getYell)
+    })
+
+    console.log(monkeyMap.get('root')())
 })
---
+++ 2022/day21/p2.js
@@ -0,0 +1,71 @@
+const fs = require('fs')
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    let lines = data.split('\r\n')
+    let monkeyMap = new Map()
+
+    lines.forEach(line => {
+        let name = line.split(':')[0]
+        let split = line.split(' ')
+        let getEquasion = function () {
+            if (name === 'humn') {
+                return 'x'
+            }
+            if (!isNaN(+split[1])) {
+                return +split[1]
+            }
+            let name1 = split[1]
+            let name2 = split[3]
+            let operator = name === 'root' ? '=' : split[2]
+
+            let equasionA = monkeyMap.get(name1).getEquasion()
+            let equasionB = monkeyMap.get(name2).getEquasion()
+            return `(${equasionA.toString().indexOf('x') === -1 ? eval(equasionA) : equasionA}${operator}${
+                equasionB.toString().indexOf('x') === -1 ? eval(equasionB) : equasionB
+            })`
+        }
+        monkeyMap.set(name, { getEquasion, getYell: () => eval(getEquasion) })
+    })
+
+    let rootEquasion = monkeyMap.get('root').getEquasion()
+    let targetNumber
+    let equasion
+
+    if (!isNaN(rootEquasion.split('=')[0])) {
+        targetNumber = +rootEquasion.split('=')[0].substring(0, rootEquasion.split('=')[1].length - 1)
+        equasion = rootEquasion.split('=')[1].substring(1)
+    } else {
+        targetNumber = +rootEquasion.split('=')[1].substring(0, rootEquasion.split('=')[1].length - 1)
+        equasion = rootEquasion.split('=')[0].substring(1)
+    }
+
+    let calculation = function (x) {
+        return eval(equasion.replace('x', x.toString()))
+    }
+
+    let correlation = calculation(1) > calculation(0) ? 1 : -1
+
+    let number = 1
+    let lastResult = calculation(number)
+    let increment = 100000000000000
+    let wasUnderTarget = true;
+    while (lastResult !== targetNumber) {
+        if (lastResult > targetNumber) {
+            if (wasUnderTarget) increment /= 10
+            wasUnderTarget = false
+            number -= increment * correlation
+        }
+        if (lastResult < targetNumber) {
+            if (!wasUnderTarget) increment /= 10
+            wasUnderTarget = true
+            number += increment * correlation
+        }
+        lastResult = calculation(number)
+    }
+
+    console.log(number)
+})
