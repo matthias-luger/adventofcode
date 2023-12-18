fd9da60a2b5703267c3f04e53e337b66eabd5b5e
Matthias Luger <malua007@live.de>
2023-12-19 00:40:02 +0100

    2023 day18

---
+++ 2023/day18/p1.js
@@ -0,0 +1,107 @@
+const fs = require('fs')
+
+fs.readFile('./input.txt', 'utf8', async (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    let instructions = parseInstructions(data.split('\r\n'))
+
+    let border = new Map()
+    let position = { x: 0, y: 0 }
+    border.set(`0,0`, '#')
+    let maxHeight = 0
+    let maxWidth = 0
+    let minHeight = Infinity
+    let minWidth = Infinity
+
+    for (const instruction of instructions) {
+        for (let i = 0; i < instruction.amount; i++) {
+            position.x += instruction.direction[0]
+            position.y += instruction.direction[1]
+
+            if (position.x > maxWidth) {
+                maxWidth = position.x
+            }
+            if (position.x < minWidth) {
+                minWidth = position.x
+            }
+            if (position.y > maxHeight) {
+                maxHeight = position.y
+            }
+            if (position.y < minHeight) {
+                minHeight = position.y
+            }
+
+            border.set(`${position.x},${position.y}`, '#')
+        }
+    }
+
+    let inside = fillInside(border, 1, 1)
+    paint(inside, border, minWidth, minHeight, maxWidth, maxHeight)
+
+    console.log(inside.size + border.size)
+})
+
+function fillInside(borderMap, x, y) {
+    let visited = new Map()
+    let queue = [`${x},${y}`]
+    while (queue.length > 0) {
+        let key = queue.shift()
+
+        if (visited.has(key)) continue
+        if (borderMap.has(key)) continue
+        let [x, y] = key.split(',').map(Number)
+
+        visited.set(key, '+')
+        queue.push(`${x + 1},${y}`)
+        queue.push(`${x - 1},${y}`)
+        queue.push(`${x},${y + 1}`)
+        queue.push(`${x},${y - 1}`)
+    }
+    return visited
+}
+
+function parseInstructions(lines) {
+    let result = []
+    const directionChars = {
+        U: [0, -1],
+        R: [1, 0],
+        D: [0, 1],
+        L: [-1, 0]
+    }
+    for (const line of lines) {
+        let [direction, amount] = line.split(' ')
+        result.push({
+            direction: directionChars[direction],
+            amount
+        })
+    }
+    return result
+}
+
+function paint(map, outside, minWidth, minHeight, width, height) {
+    let result = ''
+    for (let y = minHeight; y < height; y++) {
+        let line = ''
+        for (let x = minWidth; x < width; x++) {
+            if (x === 0 && y === 0) {
+                line += 'X'
+                continue
+            }
+
+            let val = map.get(`${x},${y}`)
+            let val2 = outside.get(`${x},${y}`)
+
+            if (val2) {
+                line += val2
+            } else if (val) {
+                line += val
+            } else {
+                line += '.'
+            }
+        }
+        console.log(line)
+    }
+    return result
+}
---
+++ 2023/day18/p2.js
@@ -0,0 +1,63 @@
+const fs = require('fs')
+
+fs.readFile('./input.txt', 'utf8', async (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    let instructions = parseInstructions(data.split('\r\n'))
+
+    let currentPoint = [0, 0]
+    let points = [currentPoint]
+
+    let U = 0
+
+    // Calculating the points in the coordinate system
+    for (const instruction of instructions) {
+        let xChange = instruction.direction[0] * instruction.amount
+        let yChange = instruction.direction[1] * instruction.amount
+
+        U += Math.abs(xChange + yChange)
+
+        currentPoint = [currentPoint[0] + xChange, currentPoint[1] + yChange]
+
+        points.push(currentPoint)
+    }
+
+    // Using the "Gaußsche Trapezformel" to calculate the area
+    let sum = 0
+    for (let i = 0; i < points.length - 1; i++) {
+        let pointA = points[i]
+        let pointB = points[i + 1]
+
+        sum += pointA[0] * pointB[1] - pointA[1] * pointB[0]
+    }
+    sum /= 2
+
+    // As these are grid points and not real coordinates, the area calculated is too small
+    // e.g. a rectange with the points (0,0), (0,1), (1,0), (1,1) should return 4 as there are 4 points, but the calculation returns 1 as this would be correct in a coordinate system
+    // To fix this, we add for each border step (circumference) 0.5 as there is technically half a grid point "outside"
+    console.log(sum + U * 0.5 + 1)
+})
+
+function parseInstructions(lines) {
+    let result = []
+    const directionChars = {
+        3: [0, -1],
+        0: [1, 0],
+        1: [0, 1],
+        2: [-1, 0]
+    }
+    for (const line of lines) {
+        let rgb = line.split(' ')[2]
+        rgb = rgb.replace(/(\(|\)|\#)/g, '')
+        let direction = rgb.substring(rgb.length - 1)
+        let amount = parseInt(rgb.substring(0, rgb.length - 1), 16)
+
+        result.push({
+            direction: directionChars[direction],
+            amount
+        })
+    }
+    return result
+}
