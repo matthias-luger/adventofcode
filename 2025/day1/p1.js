a1bd25746b91494e0c7ade38070d7c2326db2f20
Matthias Luger <malua007@live.de>
2025-12-01 10:39:57 +0100

    2025 day 1

---
+++ 2025/day1/p1.js
@@ -0,0 +1,43 @@
+const fs = require('fs')
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    let current = 50
+    let lines = data.split('\r\n')
+    let sum = 0
+
+    for (const line of lines) {
+        current = move(current, line)
+        console.log(current)
+        if (current === 0) {
+            sum++
+        }
+    }
+    console.log(sum)
+})
+
+function move(current, movement) {
+    let dir = movement[0]
+    let dist = parseInt(movement.slice(1)) % 100
+
+    let newValue = current
+    if (dir === 'L') {
+        if (current >= dist) {
+            newValue = current - dist
+        } else {
+            newValue = 100 - (dist - current)
+        }
+    }
+    if (dir === 'R') {
+        if (current + dist < 100) {
+            newValue = current + dist
+        } else {
+            newValue = current + dist - 100
+        }
+    }
+
+    return newValue
+}
---
+++ 2025/day1/p2.js
@@ -0,0 +1,56 @@
+const fs = require('fs')
+
+fs.readFile('./input.txt', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    let current = 50
+    let lines = data.split('\r\n')
+    let zeroTicks = 0
+    for (const line of lines) {
+        let [newCurent, newZeroTicks] = move(current, line)
+        console.log('current:', current, 'move:', line, 'zeroTicks this move:', newZeroTicks)
+        current = newCurent
+        zeroTicks += newZeroTicks
+    }
+    console.log(zeroTicks)
+})
+
+function move(current, movement) {
+    let dir = movement[0]
+    let dist = parseInt(movement.slice(1))
+
+    let zeroTicks = Math.floor(dist / 100)
+    dist = dist % 100
+
+    let newValue = current
+    if (dir === 'L') {
+        if (newValue >= dist) {
+            newValue = newValue - dist
+            if (newValue === 0) {
+                zeroTicks++
+            }
+        } else {
+            if (newValue !== 0) {
+                zeroTicks++
+            }
+            newValue = 100 - (dist - newValue)
+        }
+    }
+    if (dir === 'R') {
+        if (newValue + dist < 100) {
+            newValue = newValue + dist
+            if (newValue === 0) {
+                zeroTicks++
+            }
+        } else {
+            if (newValue !== 0) {
+                zeroTicks++
+            }
+            newValue = newValue + dist - 100
+        }
+    }
+
+    return [newValue, zeroTicks]
+}
