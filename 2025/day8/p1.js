7824b8cda1379c82fcc173ba3dc5a331fa6f206a
Matthias Luger <malua007@live.de>
2025-12-08 14:38:28 +0100

    2025 day 8 remove useless cache

--- 2025/day8/p1.js
+++ 2025/day8/p1.js
@@ -53,24 +53,11 @@ function calculateDistances(coords) {
     return distances.sort((a, b) => a.distance - b.distance)
 }
 
-let distanceCache = new Map()
 function distance(coords1, coords2) {
-    if (distanceCache.has(coords1) && distanceCache.get(coords1).has(coords2)) {
-        return distanceCache.get(coords1).get(coords2)
-    }
     let [x1, y1, z1] = coords1.split(',').map(x => parseInt(x))
     let [x2, y2, z2] = coords2.split(',').map(x => parseInt(x))
 
-    let dist = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2))
-
-    if (!distanceCache.has(coords1)) {
-        distanceCache.set(coords1, new Map())
-    }
-    if (!distanceCache.has(coords2)) {
-        distanceCache.set(coords2, new Map())
-    }
-    distanceCache.get(coords1).set(coords2, dist)
-    distanceCache.get(coords2).set(coords1, dist)
+    let dist = Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) + Math.pow(z2 - z1, 2)
 
     return dist
 }
